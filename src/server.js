91e1e0fef81b5c41c52eedc290338ee16a799d50
dedeusgui <guilherme.dalosto@aluno.senai.br>
2026-03-02 16:03:26 -0300

    Servidor adicionado

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Connect to MongoDB
+connectDB();
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
